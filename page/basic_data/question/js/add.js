c6d8b1e5677101f63febd9f58533bf1165677482
威利 <suyi1059882348@qq.com>
2025-08-05 16:45:08 +0800

    问题的回答改成编辑器

--- page/basic_data/question/js/add.js
+++ page/basic_data/question/js/add.js
@@ -29,4 +29,55 @@ layui.use(['form', 'table', 'myAjax'], function () {
         //更新列表
         layui.table.reload('currentTableId');
     }
+
+
+
+    ////////////////////wangeditor编辑器///////////////////////
+    const { createEditor, createToolbar } = window.wangEditor
+
+    const editorConfig = {
+        placeholder: 'Type here...',
+        MENU_CONF: {},
+        onChange(editor)
+        {
+            const html = editor.getHtml()
+            //console.log('editor content', html)
+            // 也可以同步到 <textarea>
+            document.getElementById('get_content').value = html;
+        },
+    }
+
+    // 修改 uploadImage 菜单配置
+    editorConfig.MENU_CONF['uploadImage'] = {
+        server: Requesthttp + 'index/uploadWangEditor',
+        fieldName: 'custom-name',
+        // 继续写其他配置...
+
+        //【注意】不需要修改的不用写，wangEditor 会去 merge 当前其他配置
+    }
+
+
+    const editor = createEditor({
+        selector: '#editor-container',
+        html: '<p><br></p>',
+        config: editorConfig,
+        mode: 'default', // or 'simple'
+    })
+
+    const toolbarConfig = {}
+    toolbarConfig.excludeKeys = [
+        "group-video"
+    ];//排除掉某些菜单
+    //console.log(toolbar.getConfig().toolbarKeys);
+
+    const toolbar = createToolbar({
+        editor,
+        selector: '#toolbar-container',
+        config: toolbarConfig,
+        mode: 'default', // or 'simple'
+    })
+
+
+    ////////////////////wangeditor编辑器///////////////////////
+
 });
